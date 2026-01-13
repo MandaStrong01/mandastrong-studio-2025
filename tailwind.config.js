691a9ab3efca03401785582fede919fad7a3bff0
MandaStrong01 <woolleya129@gmail.com>
2026-01-13 12:52:42 -0500

    Manda Strong Movie Studio Jan 13 2026

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        purple: {
+          600: '#8a2be2',
+        }
+      }
+    },
   },
   plugins: [],
-};
+}
