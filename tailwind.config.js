1443ced3b1d8892ab05eb7f7de6ddb68203bd111
MandaStrong01 <woolleya129@gmail.com>
2026-01-14 04:54:57 -0500

    MovieStudio

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        purple: {
-          600: '#8a2be2',
-        }
+        purple: { 600: '#8a2be2' }
       }
-    },
+    }
   },
   plugins: [],
 }
