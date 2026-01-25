d133774ca21af0d145f6abf942ac034f1a194405
MandaStrong01 <woolleya129@gmail.com>
2026-01-25 02:39:36 -0500

    Fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/mandastrong-studio-2025/',
-})
+  // THIS LINE BELOW TELLS GITHUB WHERE YOUR APP LIVES
+  base: '/mandastrong-studio-2025/', 
+  build: {
+    outDir: 'dist',
+  },
+});
