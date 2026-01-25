312905e0bbc6474a337a09f49f9874fe8847b4b6
MandaStrong01 <woolleya129@gmail.com>
2026-01-25 00:47:30 -0500

    Modify Vite config for base path and build output
    Updated base path and added build output directory.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  // THIS LINE BELOW FIXES THE 404 ERROR
+  // THIS LINE BELOW TELLS GITHUB WHERE YOUR APP LIVES
   base: '/mandastrong-studio-2025/', 
-})
+  build: {
+    outDir: 'dist',
+  },
+});
